195acce4fab325abf4e09570fa2941dd86acaece
Alvaroczrm <alvaroczrm@gmail.com>
2022-01-18 09:47:30 +0100

    finish

--- app/src/main/java/es/studium/myrecyclerview/Color.java
+++ app/src/main/java/es/studium/myrecyclerview/Color.java
@@ -4,4 +4,15 @@ public class Color {
     private final String name;
     private final String hex;
 
+    public Color(String name, String hex) {
+        this.name = name;
+        this.hex = hex;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getHex() {
+        return hex;
+    }
 }
--- app/src/main/java/es/studium/myrecyclerview/MainActivity.java
+++ app/src/main/java/es/studium/myrecyclerview/MainActivity.java
@@ -1,8 +1,17 @@
 package es.studium.myrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupReclyclerView();
+    }
+
+    private void setupReclyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        final List<Color> colors = buildColors();
+        recyclerView.setAdapter(new MaterialPaletteAdapter(colors, new RecyclerViewOnItemClickListener() {
+            @Override
+            public void onClick(View v, int position) {
+                String text = position + " " + colors.get(position).getName();
+                Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+            }
+        }));
+        //VERTICAL
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //HORIZONTAL
+        //recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL.HORIZONTAL, false));
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                ((LinearLayoutManager) recyclerView.getLayoutManager()).getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+    }
+
+    @SuppressLint("ResourceType")
+    private List<Color> buildColors() {
+        List<Color> colors = new ArrayList<>();
+        colors.add(new Color(getString(R.string.blue), getResources().getString(R.color.blue)));
+        colors.add(new Color(getString(R.string.indigo), getResources().getString(R.color.indigo)));
+        colors.add(new Color(getString(R.string.red), getResources().getString(R.color.red)));
+        colors.add(new Color(getString(R.string.green), getResources().getString(R.color.green)));
+        colors.add(new Color(getString(R.string.orange), getResources().getString(R.color.orange)));
+        colors.add(new Color(getString(R.string.bluegrey), getResources().getString(R.color.bluegrey)));
+        colors.add(new Color(getString(R.string.teal), getResources().getString(R.color.teal)));
+        colors.add(new Color(getString(R.string.deeppurple), getResources().getString(R.color.deeppurple)));
+        colors.add(new Color(getString(R.string.bluegrey), getResources().getString(R.color.bluegrey)));
+        colors.add(new Color(getString(R.string.yellow), getResources().getString(R.color.yellow)));
+        colors.add(new Color(getString(R.string.cyan), getResources().getString(R.color.cyan)));
+        colors.add(new Color(getString(R.string.brown), getResources().getString(R.color.brown)));
+        colors.add(new Color(getString(R.string.teal), getResources().getString(R.color.teal)));
+
+
+
+
+
+
+
+
+
+
+        return colors;
     }
 }
